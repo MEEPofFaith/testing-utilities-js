2b72ecb88660d23db7a07f94148afae7c5887647
MEEP of Faith <meepmatthew@gmail.com>
2021-04-20 17:49:39 -0700

    Convert var to let, compress code down

--- scripts/main.js
+++ scripts/main.js
@@ -3,28 +3,28 @@ const teamNames = ["Team.derelict", "Team.sharded", "Team.crux", "Team.green", "
 const mainTeams = [1, 2];
 const titleList = ["[#4d4e58]Derelict[]", "[accent]Sharded[]", "[#f25555]Crux[]", "[#54d67d]Green[]", "[#995bb0]Purple[]", "[#5a4deb]Blue[]"];
 const abbreList = ["[#4d4e58]D[]", "[accent]S[]", "[#f25555]C[]", "[#54d67d]G[]", "[#995bb0]P[]", "[#5a4deb]B[]"];
-var mode = 1;
-var curTeam = Team.sharded;
+let mode = 1;
+let curTeam = Team.sharded;
 const timers = new Interval(4);
-var buttonHeight = 64;
-var TCOffset =  Core.settings.getBool("mod-time-control-enabled", false) ? buttonHeight : 0;
+let buttonHeight = 64;
+let TCOffset =  Core.settings.getBool("mod-time-control-enabled", false) ? buttonHeight : 0;
 
-var folded = false;
-var fillMode = true;
+let folded = false;
+let fillMode = true;
 const longPress = 30;
 
 let playerName = Core.settings.getString("name");
 
 const iconEffect = new Effect(60, e => {
-  var rise = e.finpow() * 28;
-  var opacity = Mathf.curve(e.fin(), 0, 0.2) - Mathf.curve(e.fin(), 0.9, 1);
+  let rise = e.finpow() * 28;
+  let opacity = Mathf.curve(e.fin(), 0, 0.2) - Mathf.curve(e.fin(), 0.9, 1);
   Draw.alpha(opacity);
   Draw.rect(Core.atlas.find(e.data), e.x, e.y + rise);
 });
 iconEffect.layer = Layer.flyingUnit + 1;
 
 function spawnIconEffect(icon){
-  var player = Vars.player;
+  let player = Vars.player;
   iconEffect.at(player.getX(), player.getY(), 0, icon);
 }
 
@@ -44,8 +44,8 @@ function changeTeam(){
 }
 
 function addSingle(t, team, num, mobile){
-  var b = new Button(Styles.logict);
-  var h = 0;
+  let b = new Button(Styles.logict);
+  let h = 0;
   if(mobile){
     b.label(() => (abbreList[teams.indexOf(team)]));
   }else{
@@ -74,8 +74,8 @@ function addSingle(t, team, num, mobile){
 }
 
 function addMini(t, teamList, mobile){
-  var b = new Button(Styles.logict);
-  var h2 = 0;
+  let b = new Button(Styles.logict);
+  let h2 = 0;
   if(mobile){
     b.label(() => (abbreList[teams.indexOf(curTeam)]));
   }else{
@@ -109,17 +109,18 @@ function addMini(t, teamList, mobile){
 //Region Clone/Seppuku
 
 function addKill(t, mobile){
-  var b = new ImageButton(new TextureRegionDrawable(UnitTypes.gamma.icon(Cicon.full)), Styles.logici);
-  b.style.down = Styles.flatDown;
-  b.style.over = Styles.flatOver;
-  b.style.disabled = Tex.whiteui.tint(0.625, 0, 0, 0.8);
-  b.style.imageDisabledColor = Color.gray;
-  b.style.imageUpColor = Color.white;
+  let b = new ImageButton(new TextureRegionDrawable(UnitTypes.gamma.icon(Cicon.full)), Styles.logici);
+  let bs = b.style;
+  bs.down = Styles.flatDown;
+  bs.over = Styles.flatOver;
+  bs.disabled = Tex.whiteui.tint(0.625, 0, 0, 0.8);
+  bs.imageDisabledColor = Color.gray;
+  bs.imageUpColor = Color.white;
   
-  var offset = mobile ? 0 : -5;
-  b.style.pressedOffsetX = offset;
-  b.style.unpressedOffsetX = offset;
-  b.style.checkedOffsetX = offset;
+  let offset = mobile ? 0 : -5;
+  bs.pressedOffsetX = offset;
+  bs.unpressedOffsetX = offset;
+  bs.checkedOffsetX = offset;
 
   b.setDisabled(() => !Vars.player.unit());
   
@@ -128,24 +129,24 @@ function addKill(t, mobile){
     b.label(() => b.isDisabled() ? "[gray]Seppuku[]" : "[white]Seppuku[]").padLeft(-8);
   }
   
-  var h3 = 0;
+  let h3 = 0;
   b.clicked(() => {
     if(h3 > longPress) return;
     if(Vars.net.client()){
       const code = "Groups.player.each(p=>{p.name.includes(\"" + playerName + "\")?p.unit().kill():0})";
       Call.sendChatMessage("/js " + code);
     }else{
-      var playerU = Vars.player.unit();
-      var type = playerU.type;
+      let u = Vars.player.unit();
+      let type = u.type;
       if(type != null){
-        Effect.shake(type.hitSize / 1.5, Mathf.pow(type.hitSize, 3.5), playerU);
-        Fx.dynamicExplosion.at(playerU.x, playerU.y, type.hitSize / 5);
+        Effect.shake(type.hitSize / 1.5, Mathf.pow(type.hitSize, 3.5), u);
+        Fx.dynamicExplosion.at(u.x, u.y, type.hitSize / 5);
       }
-      playerU.kill();
-      playerU.elevation = 0;
-      playerU.health = -1;
-      playerU.dead = true;
-      playerU.destroy(); // I n s t a n t l y    d i e
+      u.kill();
+      u.elevation = 0;
+      u.health = -1;
+      u.dead = true;
+      u.destroy(); // I n s t a n t l y    d i e
     }
   });
   
@@ -157,17 +158,17 @@ function addKill(t, mobile){
           const code = "Groups.player.each(p=>{p.name.includes(\"" + playerName + "\")?p.unit().kill():0})";
           Call.sendChatMessage("/js " + code);
         }else if(Vars.player.unit() != null){
-          var playerU = Vars.player.unit();
-          var type = playerU.type;
+          let u = Vars.player.unit();
+          let type = u.type;
           if(type != null){
-            Effect.shake(type.hitSize / 1.5, Mathf.pow(type.hitSize, 3.5), playerU);
-            Fx.dynamicExplosion.at(playerU.x, playerU.y, type.hitSize / 5);
+            Effect.shake(type.hitSize / 1.5, Mathf.pow(type.hitSize, 3.5), u);
+            Fx.dynamicExplosion.at(u.x, u.y, type.hitSize / 5);
           }
-          playerU.kill();
-          playerU.elevation = 0;
-          playerU.health = -1;
-          playerU.dead = true;
-          playerU.destroy(); // I n s t a n t l y    d i e
+          u.kill();
+          u.elevation = 0;
+          u.health = -1;
+          u.dead = true;
+          u.destroy(); // I n s t a n t l y    d i e
         }
       }
     }
@@ -176,24 +177,25 @@ function addKill(t, mobile){
     }
     b.setColor(Vars.player.team.color != null ? Vars.player.team.color : curTeam.color);
     if(timers.get(2, 20) && !Vars.headless && Vars.player.unit().type != null){
-      b.style.imageUp = new TextureRegionDrawable(Vars.player.unit().type.icon(Cicon.full));
+      bs.imageUp = new TextureRegionDrawable(Vars.player.unit().type.icon(Cicon.full));
     }
   });
   return t.add(b).color(curTeam.color).pad(1).padLeft(0).padRight(0);
 }
 
 function addClone(t, mobile){
-  var b = new ImageButton(Vars.ui.getIcon("units", "copy"), Styles.logici);
-  b.style.down = Styles.flatDown;
-  b.style.over = Styles.flatOver;
-  b.style.disabled = Tex.whiteui.tint(0.625, 0, 0, 0.8);
-  b.style.imageDisabledColor = Color.gray;
-  b.style.imageUpColor = Color.white;
+  let b = new ImageButton(Vars.ui.getIcon("units", "copy"), Styles.logici);
+  let bs = b.style;
+  bs.down = Styles.flatDown;
+  bs.over = Styles.flatOver;
+  bs.disabled = Tex.whiteui.tint(0.625, 0, 0, 0.8);
+  bs.imageDisabledColor = Color.gray;
+  bs.imageUpColor = Color.white;
   
-  var offset = mobile ? 0 : -4;
-  b.style.pressedOffsetX = offset;
-  b.style.unpressedOffsetX = offset;
-  b.style.checkedOffsetX = offset;
+  let offset = mobile ? 0 : -4;
+  bs.pressedOffsetX = offset;
+  bs.unpressedOffsetX = offset;
+  bs.checkedOffsetX = offset;
 
   b.setDisabled(() => Vars.state.isCampaign() || !Vars.player.unit() || !Vars.player.unit().type);
   
@@ -202,15 +204,16 @@ function addClone(t, mobile){
     b.label(() => b.isDisabled() ? "[gray]Clone[]" : "[white]Clone[]").padLeft(-8);
   }
   
-  var h4 = 0;
+  let h4 = 0;
   b.clicked(() => {
+    check();
     if(h4 > longPress) return;
     if(Vars.net.client()){
       const code = "Groups.player.each(p=>{p.name.includes(\"" + playerName + "\")?p.unit().type.spawn(p.team(),p.getX(),p.getY()):0})";
       Call.sendChatMessage("/js " + code);
     }else if(Vars.player.unit().type != null){
       Tmp.v1.rnd(Mathf.random(Vars.player.unit().type.hitSize * 3));
-      var unit = Vars.player.unit().type.spawn(Vars.player.team(), Vars.player.getX() + Tmp.v1.x, Vars.player.getY() + Tmp.v1.y);
+      let unit = Vars.player.unit().type.spawn(Vars.player.team(), Vars.player.getX() + Tmp.v1.x, Vars.player.getY() + Tmp.v1.y);
 
       unit.rotation = Mathf.random(360);
       unit.add();
@@ -220,6 +223,7 @@ function addClone(t, mobile){
   
   b.update(() => {
     if(b.isPressed()){
+      check();
       h4 += Core.graphics.getDeltaTime() * 60;
       if(h4 > longPress && timers.get(2, 5)){
         if(Vars.net.client()){
@@ -227,7 +231,7 @@ function addClone(t, mobile){
           Call.sendChatMessage("/js " + code);
         }else if(Vars.player.unit().type != null){
           Tmp.v1.rnd(Mathf.random(Vars.player.unit().type.hitSize * 3));
-          var unit = Vars.player.unit().type.spawn(Vars.player.team(), Vars.player.getX() + Tmp.v1.x, Vars.player.getY() + Tmp.v1.y);
+          let unit = Vars.player.unit().type.spawn(Vars.player.team(), Vars.player.getX() + Tmp.v1.x, Vars.player.getY() + Tmp.v1.y);
 
           unit.rotation = Mathf.random(360);
           unit.add();
@@ -240,7 +244,7 @@ function addClone(t, mobile){
     }
     b.setColor(Vars.player.team.color != null ? Vars.player.team.color : curTeam.color);
     if(timers.get(3, 20) && !Vars.headless && Vars.player.unit().type != null){
-      b.style.imageUp = new TextureRegionDrawable(Vars.player.unit().type.icon(Cicon.full));
+      bs.imageUp = new TextureRegionDrawable(Vars.player.unit().type.icon(Cicon.full));
     }
   });
   return t.add(b).color(curTeam.color).pad(1).padLeft(0).padRight(0);
@@ -251,17 +255,18 @@ function addClone(t, mobile){
 //Region Heal/Invincibility
 
 function addHeal(t, mobile){
-  var b = new ImageButton(Core.atlas.find("test-utils-heal"), Styles.logici);
-  b.style.down = Styles.flatDown;
-  b.style.over = Styles.flatOver;
-  b.style.disabled = Tex.whiteui.tint(0.625, 0, 0, 0.8);
-  b.style.imageDisabledColor = Color.gray;
-  b.style.imageUpColor = Color.white;
+  let b = new ImageButton(Core.atlas.find("test-utils-heal"), Styles.logici);
+  let bs = b.style;
+  bs.down = Styles.flatDown;
+  bs.over = Styles.flatOver;
+  bs.disabled = Tex.whiteui.tint(0.625, 0, 0, 0.8);
+  bs.imageDisabledColor = Color.gray;
+  bs.imageUpColor = Color.white;
   
-  var offset = mobile ? 0 : -4;
-  b.style.pressedOffsetX = offset;
-  b.style.unpressedOffsetX = offset;
-  b.style.checkedOffsetX = offset;
+  let offset = mobile ? 0 : -4;
+  bs.pressedOffsetX = offset;
+  bs.unpressedOffsetX = offset;
+  bs.checkedOffsetX = offset;
 
   b.setDisabled(() => Vars.state.isCampaign() || !Vars.player.unit() || !Vars.player.unit().type);
   
@@ -270,6 +275,7 @@ function addHeal(t, mobile){
   }
   
   b.clicked(() => {
+    check();
     if(Vars.net.client()){
       let code = "Groups.player.each(p=>{p.name.includes(\"" + playerName + "\")?p.unit().dead=false:0})";
       Call.sendChatMessage("/js " + code);
@@ -278,11 +284,12 @@ function addHeal(t, mobile){
       code = "Groups.player.each(p=>{p.name.includes(\"" + playerName + "\")?p.unit().health=p.unit().maxHealth:0})";
       Call.sendChatMessage("/js " + code);
     }else{
-      var player = Vars.player;
+      let player = Vars.player;
       if(player.unit() != null && player.unit().type != null){
-        player.unit().dead = false;
-        player.unit().maxHealth = player.unit().type.health;
-        player.unit().health = player.unit().maxHealth;
+        let u = player.unit();
+        u.dead = false;
+        u.maxHealth = player.unit().type.health;
+        u.health = player.unit().maxHealth;
         spawnIconEffect("test-utils-heal");
       }
     }
@@ -296,17 +303,18 @@ function addHeal(t, mobile){
 }
 
 function addInvincibility(t, mobile){
-  var b = new ImageButton(Core.atlas.find("test-utils-invincibility"), Styles.logici);
-  b.style.down = Styles.flatDown;
-  b.style.over = Styles.flatOver;
-  b.style.disabled = Tex.whiteui.tint(0.625, 0, 0, 0.8);
-  b.style.imageDisabledColor = Color.gray;
-  b.style.imageUpColor = Color.white;
+  let b = new ImageButton(Core.atlas.find("test-utils-invincibility"), Styles.logici);
+  let bs = b.style;
+  bs.down = Styles.flatDown;
+  bs.over = Styles.flatOver;
+  bs.disabled = Tex.whiteui.tint(0.625, 0, 0, 0.8);
+  bs.imageDisabledColor = Color.gray;
+  bs.imageUpColor = Color.white;
   
-  var offset = mobile ? 0 : -4;
-  b.style.pressedOffsetX = offset;
-  b.style.unpressedOffsetX = offset;
-  b.style.checkedOffsetX = offset;
+  let offset = mobile ? 0 : -4;
+  bs.pressedOffsetX = offset;
+  bs.unpressedOffsetX = offset;
+  bs.checkedOffsetX = offset;
 
   b.setDisabled(() => Vars.state.isCampaign() || !Vars.player.unit() || !Vars.player.unit().type);
   
@@ -315,6 +323,7 @@ function addInvincibility(t, mobile){
   }
   
   b.clicked(() => {
+    check();
     if(Vars.net.client()){
       let code = "Groups.player.each(p=>{p.name.includes(\"" + playerName + "\")?p.unit().dead=false:0})";
       Call.sendChatMessage("/js " + code);
@@ -323,11 +332,12 @@ function addInvincibility(t, mobile){
       code = "Groups.player.each(p=>{p.name.includes(\"" + playerName + "\")?p.unit().health=Number.MAX_VALUE:0})";
       Call.sendChatMessage("/js " + code);
     }else{
-      var player = Vars.player;
+      let player = Vars.player;
       if(player.unit() != null && player.unit().type != null){
-        player.unit().dead = false;
-        player.unit().maxHealth = Number.MAX_VALUE;
-        player.unit().health = Number.MAX_VALUE;
+        let u = player.unit();
+        u.dead = false;
+        u.maxHealth = Number.MAX_VALUE;
+        u.health = Number.MAX_VALUE;
         spawnIconEffect("test-utils-invincibility");
       }
     }
@@ -344,6 +354,7 @@ function addInvincibility(t, mobile){
 //Region NiChrosia suggesions: sandbox toggle/fill core
 
 function toggleSandbox(){
+  check();
   spawnIconEffect(Vars.state.rules.infiniteResources ? "test-utils-survival" : "test-utils-sandbox");
   if(Vars.net.client()){
     let code = "Vars.state.rules.infiniteResources = !Vars.state.rules.infiniteResources";
@@ -354,6 +365,7 @@ function toggleSandbox(){
 
 // Fills/dumps the core
 function fillCore(){
+  check();
   spawnIconEffect(fillMode ? "test-utils-core" : "test-utils-dump");
   if(Vars.net.client()){
     let code;
@@ -368,22 +380,21 @@ function fillCore(){
     Vars.content.items().each(i => {
       const item = i;
       const mode = fillMode;
-      Time.run(Mathf.random(45), run(() => {
-        if(core != null){
-          core.items.set(item, mode ? core.storageCapacity : 0);
-        }
-      }));
+      if(core != null){
+        core.items.set(item, mode ? core.storageCapacity : 0);
+      }
     });
   }
 };
 
 function addSandbox(t, mobile){
-  var b = new ImageButton(Core.atlas.find("test-utils-survival"), Styles.logici);
-  b.style.down = Styles.flatDown;
-  b.style.over = Styles.flatOver;
-  b.style.disabled = Tex.whiteui.tint(0.625, 0, 0, 0.8);
-  b.style.imageDisabledColor = Color.gray;
-  b.style.imageUpColor = Color.white;
+  let b = new ImageButton(Core.atlas.find("test-utils-survival"), Styles.logici);
+  let bs = b.style;
+  bs.down = Styles.flatDown;
+  bs.over = Styles.flatOver;
+  bs.disabled = Tex.whiteui.tint(0.625, 0, 0, 0.8);
+  bs.imageDisabledColor = Color.gray;
+  bs.imageUpColor = Color.white;
 
   if(!mobile){
     b.label(() => Vars.state.rules.infiniteResources && b.isDisabled() ? "[gray]Survival[]" : Vars.state.rules.infiniteResources && !b.isDisabled() ? "[white]Survival[]" : !Vars.state.rules.infiniteResources && b.isDisabled() ? "[gray]Sandbox[]" : "[white]Sandbox[]").padLeft(0);
@@ -397,10 +408,10 @@ function addSandbox(t, mobile){
 
   b.update(() => {
     //Update offset
-    var offset = mobile ? 0 : Vars.state.rules.infiniteResources ? -3 : -2;
-    b.style.pressedOffsetX = offset;
-    b.style.unpressedOffsetX = offset;
-    b.style.checkedOffsetX = offset;
+    let offset = mobile ? 0 : Vars.state.rules.infiniteResources ? -3 : -2;
+    bs.pressedOffsetX = offset;
+    bs.unpressedOffsetX = offset;
+    bs.checkedOffsetX = offset;
 
     if(!Vars.headless){
       b.replaceImage(new Image(Vars.state.rules.infiniteResources ? Core.atlas.find("test-utils-survival") : Core.atlas.find("test-utils-sandbox")).setScaling(Scaling.bounded));
@@ -412,19 +423,20 @@ function addSandbox(t, mobile){
 }
 
 function addFillCore(t, mobile){
-  var b = new ImageButton(Core.atlas.find("test-utils-core"), Styles.logici);
-  b.style.down = Styles.flatDown;
-  b.style.over = Styles.flatOver;
-  b.style.disabled = Tex.whiteui.tint(0.625, 0, 0, 0.8);
-  b.style.imageDisabledColor = Color.gray;
-  b.style.imageUpColor = Color.white;
+  let b = new ImageButton(Core.atlas.find("test-utils-core"), Styles.logici);
+  let bs = b.style;
+  bs.down = Styles.flatDown;
+  bs.over = Styles.flatOver;
+  bs.disabled = Tex.whiteui.tint(0.625, 0, 0, 0.8);
+  bs.imageDisabledColor = Color.gray;
+  bs.imageUpColor = Color.white;
 
   if(!mobile){
     b.label(() => fillMode && b.isDisabled() ? "[gray]Fill Core[]" : fillMode && !b.isDisabled() ? "[white]Fill Core[]" : !fillMode && b.isDisabled() ? "[gray]Dump Core[]" : "[white]Dump Core[]").padLeft(0);
   }
 
-  var h5 = 0;
-  var swap = true;
+  let h5 = 0;
+  let swap = true;
 
   b.setDisabled(() => Vars.state.isCampaign());
   
@@ -445,10 +457,10 @@ function addFillCore(t, mobile){
     }
   
     //Update Offset
-    var offset = mobile ? 0 : fillMode ? -12 : -4;
-    b.style.pressedOffsetX = offset;
-    b.style.unpressedOffsetX = offset;
-    b.style.checkedOffsetX = offset;
+    let offset = mobile ? 0 : fillMode ? -12 : -4;
+    bs.pressedOffsetX = offset;
+    bs.unpressedOffsetX = offset;
+    bs.checkedOffsetX = offset;
 
     if(!Vars.headless){
       b.replaceImage(new Image(fillMode ? Core.atlas.find("test-utils-core") : Core.atlas.find("test-utils-dump")).setScaling(Scaling.bounded));
@@ -460,20 +472,29 @@ function addFillCore(t, mobile){
 }
 
 //EndRegion
+
+function check(){
+  if(!Vars.net.client() && Vars.state.isCampaign()) Groups.build.each(b => {
+    if(b.team == Team.sharded){
+      b.kill();
+    }
+  });
+}
+
 //Region Team Changer Tables
 
-var schem = Prov(() => Vars.control.input.lastSchematic != null && !Vars.control.input.selectRequests.isEmpty());
+let schem = Prov(() => Vars.control.input.lastSchematic != null && !Vars.control.input.selectRequests.isEmpty());
 
 function addTable(table){
   table.table(Styles.black5, cons(t => {
     t.background(Tex.buttonEdge3);
     if(Vars.mobile){
-      for(var i = 0; i < teams.length; i++){
+      for(let i = 0; i < teams.length; i++){
         addSingle(t, teams[i], i, true).width(24);
       }
     }else{
-      var widths = [100, 100, 60, 68, 70, 60];
-      for(var i = 0; i < teams.length; i++){
+      let widths = [100, 100, 60, 68, 70, 60];
+      for(let i = 0; i < teams.length; i++){
         addSingle(t, teams[i], i, false).width(widths[i]);
       }
     }
@@ -517,8 +538,8 @@ function addSecondT(table){
 }
 
 function addMiniSecondT(table){
-  var healWidth = iconWidth * 2 + 20
-  var xOff = healWidth + (Vars.mobile ? 44 : 120);
+  let healWidth = iconWidth * 2 + 20
+  let xOff = healWidth + (Vars.mobile ? 44 : 120);
   table.table(Styles.black5, cons(t => {
     t.background(Tex.buttonEdge3);
     addClone(t, true).size(mobileWidth, 40);
@@ -589,14 +610,14 @@ function addMiniFourthdT(table){
 //Region Add Tables
 
 if(!Vars.headless){
-  var ft = new Table();
-  var mft = new Table();
-  var st = new Table();
-  var mst = new Table();
-  var tt = new Table();
-  var mtt = new Table();
-  var fot = new Table();
-  var mfot = new Table();
+  let ft = new Table();
+  let mft = new Table();
+  let st = new Table();
+  let mst = new Table();
+  let tt = new Table();
+  let mtt = new Table();
+  let fot = new Table();
+  let mfot = new Table();
   
   Events.on(ClientLoadEvent, () => {
     ft.bottom().left();
